05c6bc19383f2c75e0a9404d872adc609dbd6155
brenobsd <bre.bsilva10@gmail.com>
2024-09-26 19:48:27 -0300

    login funcionando

--- script/login.js
+++ script/login.js
@@ -1,23 +1,22 @@
 const url ='https://go-wash-api.onrender.com/api/login';
-
 async function login_usuario(){
-let email = document.querySelector("#email").value;
-let password = document.querySelector("#password").value;
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
 
-if(!email){
-  alert("Preencha o campo de email")
-  return;
+  if(!email){
+    alert("Preencha o campo de email")
+    return;
   }
-if(!password){
-  alert("Preencha o campo de senha")
-  return;
+  if(!password){
+    alert("Preencha o campo de senha")
+    return;
   }
 
  let api = await fetch(url, {
     method:"POST",
     body: JSON.stringify({
-    "email": "email",
-    "password": "password",
+    "email": email,
+    "password": password,
     "user_type_id": "1",
     }),
       headers: {
@@ -28,8 +27,8 @@ if(!password){
 
 if (api.ok) {
     let resposta = await api.json();
-    alert(resposta.data);
-    console.log(resposta.data);
+    alert("Login feito com sucesso");
+    console.log("Login feito com sucesso");
     return
     } else{
     let respostaErro = await api.json();
