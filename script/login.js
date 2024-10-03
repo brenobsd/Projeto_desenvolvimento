938b345d9d1f5e1e04a6335e2ceed4143012c01f
brenobsd <bre.bsilva10@gmail.com>
2024-10-03 13:36:33 -0300

    cadastro de enderecos

--- script/login.js
+++ script/login.js
@@ -1,45 +1,43 @@
-const url ='https://go-wash-api.onrender.com/api/login';
-async function login_usuario(){
+const url = 'https://go-wash-api.onrender.com/api/login';
+async function login_usuario() {
   let email = document.querySelector("#email").value;
   let password = document.querySelector("#password").value;
 
-  if(!email){
+  if (!email) {
     alert("Preencha o campo de email")
     return;
   }
-  if(!password){
+  if (!password) {
     alert("Preencha o campo de senha")
     return;
   }
 
- let api = await fetch(url, {
-    method:"POST",
+  let api = await fetch(url, {
+    method: "POST",
     body: JSON.stringify({
-    "email": email,
-    "password": password,
-    "user_type_id": "1",
+      "email": email,
+      "password": password,
+      "user_type_id": "1",
     }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-})
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
 
-
-if (api.ok) {
-    let resposta = await api.json();
-    localStorage.setItem("user",JSON.stringify(resposta));
+  let resposta = await api.json();
+  if (api.ok) {
+    localStorage.setItem("user", JSON.stringify(resposta));
     alert("Login feito com sucesso");
-    console.log("Login feito com sucesso");
     window.location.href = "../view/home.html";
     return
-    } else{
-    let respostaErro = await api.json();
-       alert(respostaErro.data.errors)
-    }
+  } else {
+    alert(resposta.data.errors)
+    return
+  }
 }
 
-function cadastro_endereco(){
-  let user = JSON.stringify(localStorage.getItem("user"));
-  console.log(user);
+function cadastro_token(){
+  let user = JSON.parse(localStorage.getItem("user"));
+  console.log(user)
 }
-cadastro_endereco();
+cadastro_token(); 
